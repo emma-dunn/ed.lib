df9171d7706365eaf0797058203a7ca02be938fc
cuppachino <cuppachino.dev@gmail.com>
2025-06-06 18:34:29 -0500

    Add GitHub Actions workflow for deploying to GitHub Pages and update site URL in configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 // import mdx from '@astrojs/mdx';
-import markdoc from '@astrojs/markdoc'
-import sitemap from '@astrojs/sitemap';
+import markdoc from "@astrojs/markdoc";
+import sitemap from "@astrojs/sitemap";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: "https://emmadunnlib.com",
   integrations: [markdoc(), sitemap()],
 
   vite: {
